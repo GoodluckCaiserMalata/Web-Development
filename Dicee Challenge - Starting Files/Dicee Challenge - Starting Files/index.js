62ca2526f06050ef9df42518962830fb640ab07d
GoodluckCaiserMalata <g.malata@alustudent.com>
2021-08-19 09:39:38 +0200

    The Dom manipulation

--- Dicee Challenge - Starting Files/Dicee Challenge - Starting Files/index.js
+++ Dicee Challenge - Starting Files/Dicee Challenge - Starting Files/index.js
@@ -1,13 +1,14 @@
+
 var randomNumber1 = Math.floor(Math.random()*6 )+ 1;
-
-
+ 
+ 
 var randomDiceImageSource ="images/" + "dice" + randomNumber1 + ".png";
-
+ 
 var image1 = document.querySelectorAll("img")[0];
 image1.setAttribute("src", randomDiceImageSource);
-
+ 
 var randomNumber2 = Math.floor(Math.random()*6) + 1;
-
+ 
 var randomDiceImageSource2 = "images/" + "dice" + randomNumber2 + ".png";
 var image2 = document.querySelectorAll("img")[1];
 image2.setAttribute("src", randomDiceImageSource2);
