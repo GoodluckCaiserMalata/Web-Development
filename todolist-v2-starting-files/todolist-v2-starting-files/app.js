f69c88f329e805d8664f9a21aa406e44666dbb70
GoodluckCaiserMalata <g.malata@alustudent.com>
2021-09-22 07:34:55 +0200

    final version

--- todolist-v2-starting-files/todolist-v2-starting-files/app.js
+++ todolist-v2-starting-files/todolist-v2-starting-files/app.js
@@ -6,7 +6,7 @@ const mongoose =require("mongoose");
 
 const app = express();
 
-mongoose.connect("mongodb://localhost:27017/todolistDB");
+mongoose.connect("mongodb+srv://admin-goodluck:Zk7fBkwM@cluster0.wkavy.mongodb.net/todolistDB");
 
 app.set('view engine', 'ejs');
 
@@ -131,6 +131,11 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-app.listen(3000, function() {
+let port = process.env.PORT;
+if(port==null || port == ""){
+  port = 3000;
+}
+
+app.listen(port, function() {
   console.log("Server started on port 3000");
 });
